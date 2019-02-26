6258104707d8882ca1afc92052e4ee7a66e1cdfb
logarithm <iamrohan04@gmail.com>
2019-02-27 01:28:56 +0530

    completed learning

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,15 +6,19 @@ class Projects extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div class="col-md-3 col-sm-12">
+          <div class="col-md-1 col-sm-12">
             <h5>
               <bold>Education</bold>
             </h5>
           </div>
           <div class="col-md-9 col-sm-12">
             <div class="text-center">
-              <div className="alert alert-dark text-center">Undergraduate</div>
-              Sikkim Manipal Institute of Technology, Sikkim
+              <div className="text-center">
+                <span class="badge badge-pill badge-secondary">
+                  Undergraduate
+                </span>
+              </div>
+              <small>Sikkim Manipal Institute of Technology, Sikkim</small>
               <sup> 2016-2020</sup>
               <small>
                 <div class="text-muted">B.Tech (Computer Science)</div>
@@ -22,23 +26,174 @@ class Projects extends Component {
               <br />
             </div>
             <div class="text-center">
-              <div className="alert alert-dark">Senior Secondary </div>
-              Caesar School , Malbazar
+              <div className="text-center">
+                <span class="badge badge-pill badge-secondary">
+                  Senior Secondary
+                </span>
+              </div>
+              <small>Caesar School , Malbazar</small>
+              <sup> 2014-2016</sup>
               <small>
                 <div class="text-muted">(AISSCE) </div>
               </small>
               <br />
             </div>
             <div class="text-center">
-              <div className="alert alert-dark text-center">Secondary</div>
-              Caesar School , Malbazar
+              <div className="text-center">
+                <span class="badge badge-pill badge-secondary">Secondary</span>
+              </div>
+              <small>Caesar School , Malbazar</small> <sup> 2004-2014</sup>
               <small>
-                <div class="text-muted">(AISSE)</div>
+                <div class="text-muted">(AISSCE) </div>
               </small>
               <br />
             </div>
           </div>
         </div>
+
+        <hr />
+
+        {/**
+          Learning Section
+         */}
+
+        <div className="row">
+          <div class="col-md-1 col-sm-12">
+            <h5>
+              <bold>Learnings</bold>
+            </h5>
+          </div>
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">
+                Introduction to Modern App Development
+              </span>
+              <br />
+              <small>(NPTEL)</small>
+            </div>
+
+            <br />
+          </div>
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">
+                Introduction to Machine Learning
+              </span>
+              <br />
+              <small>(NPTEL)</small>
+            </div>
+
+            <br />
+          </div>
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">
+                Android App Development
+              </span>
+              <br />
+              <small>(Udemy)</small>
+            </div>
+
+            <br />
+          </div>
+        </div>
+        <hr />
+        {/**
+          Projects
+          */}
+
+        <div className="row">
+          <div class="col-md">
+            <h5>
+              <strong>Projects</strong>
+            </h5>
+          </div>
+
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">SMITConnect</span>
+              <sup> Android</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">TheConveyor</span>
+              <sup> Nodejs</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">Conneqtor</span>
+              <sup> Nodejs</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">RailWake</span>
+              <sup> Android</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">BitTrace</span>
+              <sup> Android</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">
+                TechNewsReader
+              </span>
+              <sup> Android</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">
+                Guess the Number
+              </span>
+              <sup> Javascript</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+
+          <div class="col-md">
+            <div className="text-center">
+              <span class="badge badge-pill badge-secondary">
+                Mail System Design
+              </span>
+              <sup>Algortihms</sup>
+              <br />
+            </div>
+
+            <br />
+          </div>
+        </div>
+
         <div className="cfooter">
           <Footer />
         </div>
