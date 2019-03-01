ef02bde0f6196bce950826c489531d97d7c56a8e
logarithm <iamrohan04@gmail.com>
2019-03-01 18:36:42 +0530

    added build

---
+++ build/precache-manifest.52b793da83babb566283b33b35e9f50c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bada28199115946421",
+    "url": "/static/css/main.8685a794.chunk.css"
+  },
+  {
+    "revision": "f2bada28199115946421",
+    "url": "/static/js/main.f2bada28.chunk.js"
+  },
+  {
+    "revision": "1e728f42dba9ee04beb9",
+    "url": "/static/js/1.1e728f42.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07bbe6619c5dec72a15c2d0dd7920e61",
+    "url": "/static/media/dpq.07bbe661.jpg"
+  },
+  {
+    "revision": "73cd8eea9c79cb79e61a0eec38580c4d",
+    "url": "/static/media/hello1.73cd8eea.gif"
+  },
+  {
+    "revision": "bbc6cb2d8431a576f840f91518aaf855",
+    "url": "/index.html"
+  }
+];
